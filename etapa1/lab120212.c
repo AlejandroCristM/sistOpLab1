4c91ddaa77d3a8bce81c26b06b0c05edb0776c30
cristiancastano852 <44209773+cristiancastano852@users.noreply.github.com>
2021-12-14 18:04:39 -0500

    Input de prueba, se agrega codigo para leer proc, el ejecutable es lo que nos crea cuando corremos el programa

--- etapa1/lab120212.c
+++ etapa1/lab120212.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 int main() {
-  printf("HelloWorld\n");
+
+  char filename[] = "/proc/2/status";
+  char line[201];
+  int numLinea = 1;
+  FILE *iF;  
+  iF = fopen(filename,"r");
+  if (iF == NULL) {
+    printf("Error al abrir el archivo %s\n", filename);
+    exit(-1);
+  }
+  while(fgets(line, 201, iF)!=NULL) {
+    printf("%-5d",numLinea++);
+    printf("%s",line);
+  } 
+  fclose(iF);
   exit(0);
+
+  //aaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+    
 }
