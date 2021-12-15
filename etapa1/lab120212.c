94922d868bf9f44c7be7011870de0113a7a507fe
Karol Alzate <karol.alzate@udea.edu.co>
2021-12-14 21:20:51 -0500

    arreglar concatenación

--- etapa1/lab120212.c
+++ etapa1/lab120212.c
@@ -5,40 +5,31 @@
 //#include "getProcInfo.h"
  
 
-int main(){
-    char str[3];
-    //printf("%s", str);
-    char conca[]="/proc/";
-    char conca2[]="/status";
-   // printf("%s",conca);
-   // printf("%s","    ");
-    fgets(str, 3, stdin);
-    strcat(conca,str);
-   // strcat(conca,conca2);
-    strcat(conca,conca2);
-    //printf("%s",conca);
+int main(int argc, char *argv[]){
+    char filename[20];
+    strcpy(filename, "/proc/");
+    strcat(filename, argv[1]);
+    strcat(filename, "/status");
+    printf("%s",filename);
+    printf("\n");
 
-    char str1[80];
-    strcpy(str1, "/proc/");
-    strcat(str1, str);
-    strcat(str1, "/status");
-    printf("%s",str1);
-    // char filename[] = conca;
-    // char line[201];
-    // int numLinea = 1;
-    // FILE *iF;  
-    // iF = fopen(filename,"r");
-    // if (iF == NULL) {
-    // printf("Error al abrir el archivo %s\n", filename);
-    // exit(-1);
-    // }
-    // while(fgets(line, 201, iF)!=NULL) {
-    // printf("%-5d",numLinea++);
-    // printf("%s",line);
-    // } 
-    // fclose(iF);
-    // exit(0);
-    // }
+    //char filename[80] = str;
+    char line[201];
+    int numLinea = 1;
+    FILE *iF;  
+    iF = fopen(filename,"r");
+    if (iF == NULL) {
+      printf("Error al abrir el archivo %s\n", filename);
+      exit(-1);
+    }
+    while(fgets(line, 201, iF)!=NULL) {
+      printf("%-5d",numLinea++);
+      printf("%s",line);
+    } 
+    
+    fclose(iF);
+    exit(0);
+    
 
     
 }
