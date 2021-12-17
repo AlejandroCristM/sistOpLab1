18c8e7bab7304d9d5ed532ac661b281341bc923d
Karol Alzate <karol.alzate@udea.edu.co>
2021-12-17 16:32:38 -0500

    separar métodos en .h

--- etapa1/lab120212.c
+++ etapa1/lab120212.c
@@ -2,59 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-//#include "getProcInfo.h"
+#include "methods.h"
  
-char *remove_word(char *str, const char *sub) {
-    int len = strlen(sub);
-    char *q;
-    if (len > 0) {
-        char *p = str;
-        q = p + len + 1;
-    }
-    return q;
-}
 
 int main(int argc, char *argv[]){
     char filename[20];
     strcpy(filename, "/proc/");
     strcat(filename, argv[1]);
     strcat(filename, "/status");
+    /*char *filename;
+    filename = concatenateFilename(argv[1]);
+    printf("%s", filename);*/
+    
+    FILE *iF = openFile(filename); 
 
-    char line[201];
-    char *result, *resultVmData, *resultVmStk;
-    FILE *iF;  
-    iF = fopen(filename,"r");
-    if (iF == NULL) {
-      printf("Error al abrir el archivo %s\n", filename);
-      exit(-1);
-    }
-    while(fgets(line, 201, iF)!=NULL) {
-      if (strstr(line, "Name")) {
-        result = remove_word(line, "Name");
-        printf("Nombre del proceso:%s", result);
-      } else if (strstr(line, "State")){
-        result = remove_word(line, "State");
-        printf("Estado:%s", result);
-      } else if (strstr(line, "VmSize")){
-        result = remove_word(line, "VmSize");
-        printf("Tamaño total de la imagen en memoria:%s", result);
-      } else if (strstr(line, "VmData")){
-        resultVmData = remove_word(line, "VmData");
-      } else if (strstr(line, "VmStk")){
-        resultVmStk = remove_word(line, "VmStk");
-      } else if (strstr(line, "VmExe")){
-        result = remove_word(line, "VmExe");
-        printf("\tTamaño de la memoria en la región TEXT:%s", result);
-        printf("\tTamaño de la memoria en la región DATA:%s", resultVmData);
-        printf("\tTamaño de la memoria en la región STACK:%s", resultVmStk);
-      } else if (strstr(line, "voluntary_ctxt_switches") && !strstr(line, "nonvoluntary_ctxt_switches")){
-        result = remove_word(line, "voluntary_ctxt_switches");
-        printf("Número de cambios de contexto realizados (voluntarios - no voluntarios): %s -", result);
-      } else if (strstr(line, "nonvoluntary_ctxt_switches")){
-        result = remove_word(line, "nonvoluntary_ctxt_switches");
-        printf("%s", result);
-      }
-    } 
+    printInformation(iF);
     
     fclose(iF);
     exit(0);
---
+++ etapa1/methods.h
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+char *remove_word(char *str, const char *sub) { // mueve el apuntador de la línea (str) hasta que no esté el parámetro (sub).
+    int len = strlen(sub);
+    char *q;
+    if (len > 0) {
+        char *p = str;
+        q = p + len + 1;
+    }
+    return q;
+}
+
+char *concatenateFilename(char *processId){
+    char filename2[20], *p;
+    strcpy(filename2, "/proc/");
+    strcat(filename2, processId);
+    strcat(filename2, "/status");
+    p = filename2;
+    return p;
+}
+
+
+FILE *openFile(char *filename){
+    FILE *iF = fopen(filename,"r");
+    if (iF == NULL) {
+      printf("Error al abrir el archivo %s\n", filename);
+      exit(-1);
+    }
+    return iF;
+}
+
+void printInformation(FILE *iF){
+    char line[201];
+    char *result, *resultVmData, *resultVmStk;
+    while(fgets(line, 201, iF)!=NULL) {
+        if (strstr(line, "Name")) {
+            result = remove_word(line, "Name");
+            printf("Nombre del proceso:%s", result);
+        } else if (strstr(line, "State")){
+            result = remove_word(line, "State");
+            printf("Estado:%s", result);
+        } else if (strstr(line, "VmSize")){
+            result = remove_word(line, "VmSize");
+            printf("Tamaño total de la imagen en memoria:%s", result);
+        } else if (strstr(line, "VmData")){
+            resultVmData = remove_word(line, "VmData");
+        } else if (strstr(line, "VmStk")){
+            resultVmStk = remove_word(line, "VmStk");
+        } else if (strstr(line, "VmExe")){
+            result = remove_word(line, "VmExe");
+            printf("\tTamaño de la memoria en la región TEXT:%s", result);
+            printf("\tTamaño de la memoria en la región DATA:%s", resultVmData);
+            printf("\tTamaño de la memoria en la región STACK:%s", resultVmStk);
+        } else if (strstr(line, "voluntary_ctxt_switches") && !strstr(line, "nonvoluntary_ctxt_switches")){
+            result = remove_word(line, "voluntary_ctxt_switches");
+            printf("Número de cambios de contexto realizados (voluntarios - no voluntarios): %s -", result);
+        } else if (strstr(line, "nonvoluntary_ctxt_switches")){
+            result = remove_word(line, "nonvoluntary_ctxt_switches");
+            printf("%s", result);
+      }
+    }
+}
